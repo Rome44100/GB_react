6d8253af575a12b68b995201cba7ff6e3dbd7414
Rome44100 <rome44100@gmail.com>
2021-08-02 18:35:52 +0300

    Correct Lesson 5

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -1,28 +1,35 @@
 import React, { useCallback } from "react";
 
+import { useDispatch, useSelector } from "react-redux";
+
 import { store } from "../../store/index"
-import { changeName } from "../../store/actions/profile";
+import { changeIsOnline, changeName } from "../../store/actions/profile";
 
 export default function Profile() {
-    // const [ dummy, setDummy ] = React.useState();
-    const { showName, name } = store.getState().profile;
-    const dispatch = store.dispatch;
+    // const { showName, name } = store.getState().profile;
+    // const dispatch = store.dispatch;
 
-    const setShowName = useCallback(() => {
-        dispatch(changeName);
-        // setDummy({});
-    }, [dispatch]);
-    
+    const dispatch = useDispatch();
+    const { age, name, isOnline } = useSelector( state => state.profile )
+
+    const handleIsOnline = (e) => {
+        dispatch(changeIsOnline(e.target.checked))
+    }    
 
     return <div>
             <h3>Profile</h3>
+            <p>
+                <b>Name:</b> { name }
+            </p>
+            <p>
+                <b>Age:</b> { age }
+            </p>
             <input 
                 type="checkbox"
-                checked={ showName }
-                value={ showName }
-                onChange={ setShowName }
+                checked={ isOnline }
+                // value={ isOnline }
+                onChange={ handleIsOnline }
                 />
-            <span>Show Name</span>
-            { showName && <div>{name}</div> }
+            <span>Set Online</span>
         </div>
 }
--- src/store/reducers/profile.js
+++ src/store/reducers/profile.js
@@ -1,9 +1,10 @@
-import { CHANGE_NAME } from "../actions/profile.js";
+import { CHANGE_IS_ONLINE, CHANGE_NAME } from "../actions/profile.js";
 
 const initialState = {
-    showName: false,
+    // showName: false,
     name: "Niburu",
-    age: 237
+    age: 237,
+    isOnline: true
 }
 
 export default function profileReducer(state = initialState, action) {
@@ -11,8 +12,14 @@ export default function profileReducer(state = initialState, action) {
         case CHANGE_NAME: {
             return {
                 ...state,
-                name: action.payload.name,
-                showName: !state.showName
+                name: action.payload.name
+                // showName: !state.showName
+            }
+        }
+        case CHANGE_IS_ONLINE: {
+            return {
+                ...state,
+                isOnline: action.payload.isOnline
             }
         }
         default:
